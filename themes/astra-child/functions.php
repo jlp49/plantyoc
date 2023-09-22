f8a0a00a366280d9b6ed254d536eaae1f87b8177
jlp49 <jeanlouis.pare@free.fr>
2023-09-22 10:00:27 +0200

    first commit

---
+++ themes/astra-child/functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+// HOOK ADMIN
+
+
+
+//function add_admin_link( $items, $args ) {
+ //   if (is_user_logged_in() && $args->theme_location == 'primary') {
+//        $items .= '<li class="admin"><a href="'. get_admin_url() .'">Admin</a></li>';
+//    }
+  //  return $items;
+//}
+//add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
